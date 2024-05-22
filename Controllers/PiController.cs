38fa2990db5616378db0ad067555dd3a1b1f4c53
jerapiblaze <tddt0310@gmail.com>
2024-05-22 18:26:38 +0700

    Add project files.

---
+++ Controllers/PiController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Concurrent;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace loadbalancer_simpleapp_csharp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PiController : ControllerBase
+    {
+        // GET: api/<PiController>
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET api/<PiController>/5
+        [HttpGet("{steps}")]
+        public IResult Get(int steps)
+        {
+            if (steps <= 0 || steps > 999999999)
+            {
+                return Results.BadRequest("0 < n < 999999999");
+            }
+            double sum = 0.0;
+            double step = 1.0 / steps;
+            object obj = new();
+            Parallel.ForEach(
+                Partitioner.Create(0, steps),
+                () => 0.0,
+                (range, state, partial) =>
+                {
+                    for (int i = range.Item1; i < range.Item2; i++)
+                    {
+                        double x = (i - 0.5) * step;
+                        partial += 4.0 / (1.0 + x * x);
+                    }
+
+                    return partial;
+                },
+                partial => { lock (obj) sum += partial; }
+            );
+
+            return Results.Ok((step * sum).ToString());
+        }
+
+        // POST api/<PiController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        // PUT api/<PiController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        // DELETE api/<PiController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ Controllers/RecurseController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace loadbalancer_simpleapp_csharp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecurseController : ControllerBase
+    {
+        // GET: api/<RecurseController>
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET api/<RecurseController>/5
+        [HttpGet("{n}")]
+        public IResult Get(int n)
+        {
+            if (n <= 0 || n > 24)
+            {
+                return Results.BadRequest("0 < n < 25");
+            }
+            var list = (List<Int128>)[0, 1, 2, 3, 4, 5];
+            var output = Recursive(list, n);
+            return Results.Ok(string.Join(",", output.GetRange(0, 10000)) + "...");
+        }
+
+        public static Int128 Factorial(Int128 n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+ 
+            return n * Factorial(n - 1);
+        }
+        public static List<Int128> Recursive(List<Int128> list, Int128 n)
+        {
+            if (n <= 0)
+            {
+                return list;
+            }
+            var j = list.Count;
+            var a = list;
+            for (var i = 0; i < j; i++)
+            {
+                a.Add(Factorial(n));
+            }
+            n--;
+            Recursive(list, n);
+            return list;
+        }
+
+        // POST api/<RecurseController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        // PUT api/<RecurseController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        // DELETE api/<RecurseController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
